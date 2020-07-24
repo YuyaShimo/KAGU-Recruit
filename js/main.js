cdc059701c49144eb4136dc0050a5fa6abd6430c
mei0225 <mei300225@gmail.com>
2020-07-24 21:17:56 +0900

    slicksliderを実装

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+  $('#slider-list').on('init',function(){
+    $('.slider-info').addClass('on');
+  });
+    $('#slider-list').slick({
+      autoplay:true,
+      autoplaySpeed:5000,
+      dots:true,
+      infinite:true,
+    });
+    $('#slider-list').on('beforeChange',function(){
+      $('.slider-info').removeClass('.on');
+    });
+    $('#slider-list').on('afterChange',function(){
+      $('.slider-info').addClass('.on');
+    });
+});
